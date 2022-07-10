544eb5c253842d22db22c10efa6e1585574761a0
sabinary <sabina.matjasic@gmail.com>
2022-07-10 13:09:38 +0200

    Add twoSum solution

---
+++ src/leetcode/solutions/TwoSum.java
@@ -0,0 +1,30 @@
+package leetcode.solutions;
+
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (i + 1 < nums.length) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String args[]) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.print(Arrays.toString(twoSum(nums, target)));
+    }
+}
+
+
+
+
+
